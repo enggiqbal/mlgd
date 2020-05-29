22fadf5afb158d4fe85c28d764d3a5bac2642078
enggiqbal <enggiqbal@gmail.com>
2020-05-29 13:23:06 -0700

    added comments and info

--- ml_tree_extractor/degree_reducer.py
+++ ml_tree_extractor/degree_reducer.py
@@ -1,9 +1,12 @@
+#idea of degree reduce comes from Faryad Darabi Sahneh 
+#written by Iqbal Hossain
+
 import networkx as nx
 import pygraphviz as pgv
 import argparse
 from networkx.drawing.nx_agraph import write_dot
 
- 
+#reduce degree by using ZTest with cuttoff 50
 def graph_reducer(G):
     node_frequency = nx.get_node_attributes(G, 'weight')
     total = sum(int(node_frequency[k]) for k in node_frequency.keys())
@@ -22,27 +25,8 @@ def graph_reducer(G):
     Gc = max(nx.connected_components(G), key=len)
     G = nx.subgraph(G, Gc)
     return G
+ 
 
-def degree_distribution(G,name):
-    import collections
-    import matplotlib.pyplot as plt
-    degree_sequence = sorted([d for n, d in G.degree()], reverse=True) 
-    degreeCount = collections.Counter(degree_sequence)
-    deg, cnt = zip(*degreeCount.items())
-    plt.clf()
-    fig, ax = plt.subplots()
-    plt.bar(deg, cnt, width=0.80, color='b')
-    plt.title("Degree Histogram")
-    plt.ylabel("Count")
-    plt.xlabel("Degree")
-    ax.set_xticks([d + 0.4 for d in deg])
-    ax.set_xticklabels(deg)
-    plt.savefig(name+".png")
-    
-
-
-
-    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='graph reducer')
@@ -54,10 +38,9 @@ if __name__ == "__main__":
                         help='Output dot path', required=True)
     args = parser.parse_args()
     G = nx.Graph(pgv.AGraph(args.input_dot))
-    #degree_distribution(G,"old")
     print("old max degree:", max([d for n, d in G.degree()]))
     G=graph_reducer(G)
     print("new max degree:", max([d for n, d in G.degree()]))
     write_dot(G,args.output_dot)
-    #degree_distribution(G,"new")
+ 
  
